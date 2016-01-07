4342427d65b2b50f204ebee60849fb20755ebdb6
Jack-Herrer <michielvanderlist@yahoo.co.uk>
2016-01-07 14:47:38 +0100

    Prototype version: listener to atm input box

--- BB/app/src/main/java/com/example/jackherrer/bb/AtmActivity.java
+++ BB/app/src/main/java/com/example/jackherrer/bb/AtmActivity.java
@@ -6,10 +6,14 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class AtmActivity extends AppCompatActivity {
 
@@ -20,13 +24,73 @@ public class AtmActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        final double exchangeRate = 0.8;
+        final String homeCurrency = "€";
+
 
         SharedPreferences values = getSharedPreferences("values", Context.MODE_PRIVATE);
-        double bank_balance =  getDouble(values, "bank_balance", 0.00);
+        final double bank_balance =  getDouble(values, "bank_balance", 0.00);
+        final TextView homeCurrencyView = (TextView) findViewById(R.id.atm_amount_entered_in_own_currency);
+        final TextView balanceAfterView = (TextView) findViewById(R.id.atm_balance_after_withdrawal);
 
         TextView balanceView = (TextView) findViewById(R.id.atm_current_balance);
         balanceView.setText("" + bank_balance);
+
+
+
+        final EditText inputBox = (EditText) findViewById(R.id.atm_withdrawal_input);
+
+        inputBox.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+//
+                try {
+                    double inputAmount = Double.parseDouble(inputBox.getText().toString());
+                    homeCurrencyView.setText(homeCurrency+ " " + inputAmount * exchangeRate);
+                    balanceAfterView.setText(homeCurrency + " " + (bank_balance - (inputAmount * exchangeRate)));
+
+                } catch (final NumberFormatException e) {
+
+                    //catch non doubles
+
+                }
+
+            }
+        });
     }
+
+
+
+//    et1.addTextChangedListener(new TextWatcher() {
+//        @Override
+//        public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            // TODO Auto-generated method stub
+//        }
+//
+//        @Override
+//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            // TODO Auto-generated method stub
+//        }
+//
+//        @Override
+//        public void afterTextChanged(Editable s) {
+//
+//            // TODO Auto-generated method stub
+//        }
+//    });
 
     double getDouble(final SharedPreferences prefs, final String key, final double defaultValue) {
         if (!prefs.contains(key))
--- PROCESS.md
+++ PROCESS.md
@@ -8,6 +8,8 @@ Proposal and desigh sketches
 
 # day 3
 
+Basic interface development
+
 ## Design choices
 
 Start with ATM function for simple prototype
@@ -17,3 +19,13 @@ Target more users by changing project into Budget Boss instead of Backpacker's B
 Budget Boss name is already taken
 
 Idea for first version: app will focus on bank transactions and visiualise how much of the budget is left while automatically converting currencies, using exchange rate api
+
+# day 4
+
+Advanced interface development
+
+## Design choices
+
+App idea changed. App will be a helper to give all relevant information for atm transactions in foreighn countries instead of a budget monitor. Bacause of this, the app will be a lot more simplistic and more relevant than the original idea.
+
+Listener implemented to edittext. User can see changes as the user is typing, no need to press an extra button to see effect
